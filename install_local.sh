2f2b78f60903f2a749597ad2b8c268b3ead12e34
Nami-R2301 <nami.reghbati@gmail.com>
2023-05-09 20:24:13 -0400

    Added scripts for installation and uninstallation. Added install prefix for CMakeLists.txt.

---
+++ install_local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Build using CMake
+mkdir -p build
+cmake -S . -B build
+cd build
+cmake --build . --target Editor
+
+# Setup working directory for Engine to load assets properly
+sudo mkdir -p /usr/local/Wave-Engine/bin
+sudo cp -R Editor /usr/local/Wave-Engine/bin
+sudo mkdir -p /usr/local/Wave-Engine/Wave && sudo mkdir -p /usr/local/Wave-Engine/Wave-Editor
+sudo cp -R ../Wave/res/ /usr/local/Wave-Engine/Wave
+sudo cp -R ../Wave-Editor/res/ /usr/local/Wave-Engine/Wave-Editor
+
+# Setup permissions to be able to read ressources and write to imgui.ini and wave-engine.ini
+sudo chmod -R +rx /usr/local/Wave-Engine/*
+sudo chmod -R +rwx /usr/local/Wave-Engine/bin
+
+# Setup shortcut for Wave-Engine's executable
+sudo echo "#!/bin/bash" >> /usr/local/bin/wave-engine
+sudo echo "cd /usr/local/Wave-Engine/bin" >> /usr/local/bin/wave-engine
+sudo echo "./Editor" >> /usr/local/bin/wave-engine
+sudo echo 'exit $?' >> /usr/local/bin/wave-engine
+sudo chmod +rwx /usr/local/bin/wave-engine
+
+exit 0
